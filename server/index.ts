436a8bb516c7546b0ab850214a7d1011595826f5
samuelwondimu <wsamuelwondimu@gmail.com>
2021-10-28 22:24:12 +0300

    setup server index.ts file

--- server/index.ts
+++ server/index.ts
@@ -1,22 +1,73 @@
-import express, { Application, Request, Response } from "express";
+import express, { Application, Request, Response, Router } from "express";
+import http from "http";
+import bodyParser from "body-parser";
+import logging from "./config/logging";
+import config from "./config/config";
 
-const app: Application = express();
+const NAMESPACE = "Server";
+const router: Application = express();
 const port = 3000;
 
-// Body parsing Middleware
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+// log the request
+router.use((req, res, next) => {
+  /** Log the req */
+  logging.info(
+    NAMESPACE,
+    `METHOD: [${req.method}] - URL: [${req.url}] - IP: [${req.socket.remoteAddress}]`
+  );
 
-app.get("/", async (req: Request, res: Response): Promise<Response> => {
+  res.on("finish", () => {
+    /** Log the res */
+    logging.info(
+      NAMESPACE,
+      `METHOD: [${req.method}] - URL: [${req.url}] - STATUS: [${res.statusCode}] - IP: [${req.socket.remoteAddress}]`
+    );
+  });
+});
+
+// Body parsing Middleware
+router.use(express.json());
+router.use(express.urlencoded({ extended: true }));
+
+// rules of api
+router.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+  );
+
+  if (req.method == "OPTIONS") {
+    res.header("Access-Control-Allow-Methods", "PUT, POST, PATCH, DELETE, GET");
+    return res.status(200).json({});
+  }
+
+  next();
+});
+
+// routes
+// router.use("/users", userRoutes);
+
+router.get("/", async (req: Request, res: Response): Promise<Response> => {
   return res.status(200).send({
     message: "Hello World!",
   });
 });
 
-try {
-  app.listen(port, (): void => {
-    console.log(`Connected successfully on port ${port}`);
+// error handling
+router.use((req, res, next) => {
+  const error = new Error("Not found");
+
+  res.status(404).json({
+    message: error.message,
   });
-} catch (error) {
-  console.error(`Error occured: `);
-}
+});
+
+const httpServer = http.createServer(router);
+
+httpServer.listen(config.server.port, () =>
+  logging.info(
+    NAMESPACE,
+    `Server is running ${config.server.hostname}:${config.server.port}`
+  )
+);
